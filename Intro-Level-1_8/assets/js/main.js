4217008bcc02f1d4ce8b66cc83d1e446d21ab433
[PeterWolf242] <[peter.ebm@web.de]>
2024-12-09 15:02:32 +0100

    arithmetische_operatoren

---
+++ Intro-Level-1_8/assets/js/main.js
@@ -0,0 +1,49 @@
+let x=20;
+
+let y=30;
+
+console.log(x+y);
+
+console.log(x-y);
+
+console.log(y-x);
+
+console.log(x*y);
+
+console.log(x/y);
+
+let z = 10;
+
+let resultOne=((x*y) / 10);
+
+console.log(resultOne);
+
+let a = 15;
+
+let b = 9;
+
+let difference = a % b;
+
+console.log(difference);
+
+let c = 20;
+
+console.log(resultTwo =  (a + b) * c);
+
+a++;
+
+console.log(a);
+
+b--;
+
+console.log(b);
+
+let resultThree = a - b;
+
+console.log(Rest = resultOne % resultTwo);
+
+
+
+
+
+
